e79e32c3ae1c0ad8000dd0792b497c1d0a05ffb4
Qiaozhi Wang <qwang97@1005b-10.eecs.ku.edu>
2016-04-11 23:26:58 -0500

    Seperate to different small files

---
+++ check_conflict.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pprint as pp 
+from io import StringIO
+import re, timeit, collections, math
+from ad_sets import ad_sets, A_set
+
+def check_conflict(vectors, d):	#vectors-contain decisions
+	data_mat = vectors[:,0:-1]
+	d_star = d
+	non_cf = []
+	conflict = False
+	BigA = A_set(data_mat)
+	#print("BigA:")
+	#print(BigA)
+	#print("d_set:")
+	#print(d_set)
+	start = timeit.default_timer()
+	fullSet = [i for i in range(0, len(data_mat[:,1]))]
+	for sub_A in BigA:
+		for sub_d in d_star:
+			if set(sub_A) == set(np.intersect1d(sub_A, sub_d)):
+				non_cf.extend(sub_A)
+				#print(non_cf)
+	conflictSet = np.setdiff1d(fullSet,non_cf)
+	if len(conflictSet):
+		conflict = True
+	stop = timeit.default_timer() 
+	print("conflict: ", conflict)
+	print("conflictSet:")
+	print(conflictSet)
+	print("**Time of check conflict: ", stop-start)
+	return conflict, conflictSet 
---
+++ cutpoint.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pprint as pp 
+from io import StringIO
+import re, timeit, collections, math
+from ad_sets import ad_sets, A_set
+
+def entropy(mat, cf_set, total_dict, col_k_dict):
+	print("**********Entropy function*****************")
+	new = np.array([mat[i][:] for i in cf_set])
+	col_num = len(new[1,:])-1
+	case_num = len(new[:,1])
+	print(col_num)
+	cf_num = len(cf_set)
+	print(new)
+	total_entrp = {}
+	for i in range(0, col_num):	#
+		column = new[:,i]
+		uniques, counts = np.unique(column, return_counts=True)
+		col_dict = dict(zip(uniques, counts))
+		col_entrp = 0
+		print(col_dict)
+		for key, value in col_dict.items():
+			P = float(value)/case_num
+			print("P: %s/%s"%(value,case_num))
+			poss = np.where((column==key))
+			val_decisions = [new[:,-1][pos] for pos in poss]
+			u, c = np.unique(val_decisions, return_counts=True)
+			val_dict = dict(zip(u,c))
+			val_sum = 0
+			for k, v in val_dict.items():
+				p = float(v)/value
+				print("p: %s/%s"%(v,value))
+				val_sum += p*(math.log(p,2))
+			col_entrp += -(P*val_sum)
+		print("col_entrp: ", col_entrp)
+		total_entrp[i] = col_entrp
+	print("total_entrp: ", total_entrp)
+	#--------------sort by entropy---------#
+	s_entrp = sorted(total_entrp.items(), key=lambda x: x[1])
+	print("s_entrp; ", s_entrp)
+	#-------if same entropy, select smallest column order--------#
+	#---and make sure there possibility to get another cutpoint--#
+	for order in range(0, col_num):
+		candi_set = [r for r in range(0, col_num) if s_entrp[order][1] == s_entrp[i][1])
+		scandi_set = sorted(candi_set)
+		for s in range(0, len(scandi_set)):
+			
+		if not comp:
+			break
+		candi = min(selected_col, s_entrp[order][0])
+		if len(total_dict[candi]) == col_k_dict[candi]-1:
+			
+	print("selected_col: ", selected_col)
+	
+	return selected_col
+	
+def cutpoint_again(column, cutpoints_dict, k_int):
+	#----------------find the cutpoint-----------------#
+	
+	
+	return new_column
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
